724090ff8b180a44abafa50a7c8ae504fb366442
Drew Buckman <drew.buckman@gmail.com>
2020-04-30 13:42:47 -0400

    shifting config

--- scripts/updateKube.sh
+++ scripts/updateKube.sh
@@ -1,7 +1,2 @@
 #!/bin/sh
 sed -i "s/{{VERSION}}/${TRAVIS_BUILD_NUMBER}/g" kube/deployment.yml
-sed -i "s/{{BASE_URL}}/${BASE_URL}/g" kube/deployment.yml
-sed -i "s/{{ARANGODB_SETTINGS}}/${ARANGODB_SETTINGS}/g" kube/deployment.yml
-sed -i "s/{{FIREBASE_CONFIG}}/${FIREBASE_CONFIG}/g" kube/deployment.yml
-sed -i "s/{{SPACES_KEY}}/${SPACES_KEY}/g" kube/deployment.yml
-sed -i "s/{{SPACES_SECRET}}/${SPACES_SECRET}/g" kube/deployment.yml
